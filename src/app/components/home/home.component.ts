400a9d7db54f8cf42c82cc206d18164ee145b173
chie-hira <capella.a36@gmail.com>
2024-09-14 21:10:58 +0900

    ngForディレクティブでcategoryボタンを表示する

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // 本来はAPIから取得するデータ
+  public categories: string[] = ['Fridge', 'Washer', 'Kitchen', 'Vacuum', 'Climate', 'TV', 'Other'];
 
+  // 一意の識別子を返すメソッド
+  //ngForディレクティブでリストを描画する際に使用
+  public trackByIndex(index: number, item: any): number {
+    return index;
+  }
 }
