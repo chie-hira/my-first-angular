b59c0660ebf204163c086017267c3c64397ab528
chie-hira <capella.a36@gmail.com>
2024-09-15 13:51:17 +0900

    カートに商品追加機能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // ディレクティブを使用するために必要
+import { ProductService } from './services/product.service';
+import { Product } from './models/product.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'my-first-angular';
+  public title = 'my-first-angular';
+  public isCartVisible: boolean = false;
+  public cartItems: Product[] = [];
+  private addToCartSubscription: Subscription;
+
+  constructor(private productService: ProductService) {
+    this.addToCartSubscription = this.productService.onAddToCart.subscribe(
+      (product: Product) => {
+        this.cartItems.unshift(product); // リストの先頭に追加
+        // this.cartItems.push(product); //
+      }
+    );
+  }
+
+  // メモリリーク(メモリの不要な消費)の防止
+  // subscriptionは自動で解除されないため、明示的にunsubscribe()を呼び出す必要がある
+  public ngOnDestroy(): void {
+    if (this.addToCartSubscription) {
+      this.addToCartSubscription.unsubscribe();
+    }
+  }
+
+  // 表示非表示の切り替え
+  public showCart(): void {
+    this.isCartVisible = !this.isCartVisible;
+  }
+
+  public trackByIndex(index: number, item: Product): number {
+    return index;
+  }
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Inject } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   // selectorがコンポートのHTML要素
@@ -12,8 +13,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-card.component.css'
 })
 
+// これはinputデコレータを使用した単純なデータの受け渡し(直接送受信)
+// コンポーネントの使い回しが生じる大規模開発の場合は、サービスを使用してデータを受け渡す(間接送受信)
 // 親コンポーネントから受け取るデータをInputデコレータで定義
 // これで子コンポーネント内でproductプロパティを使用できる
 export class ProductCardComponent {
   @Input() product!: Product;
+
+  // コンストラクタを使用したDI(Dependency Injection)の実装
+  constructor(private productService: ProductService) {}
+
+  // Inject()関数を使ったフィールドインジェクション
+  // private productService = Inject(ProductService);
+
+  public addToCart(addedProduct: Product): void {
+    this.productService.onAddToCart.next(addedProduct);
+  }
 }
